3c87085e7d0f544466766861fba715b7029bb0ef
Andrea <Usuario@192.168.1.10>
2017-11-03 19:22:25 -0600

    Encabezado en GUI

--- Laboratorio5/src/GuiPrincipal.java
+++ Laboratorio5/src/GuiPrincipal.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * GuiPrincipal.java
+ * Interfaz grafica de ingreso de datos
+ * Andrea Arguello 17801 Alejandro Tejada 17584
+ * 3/11/2017
  */
 
 /**
